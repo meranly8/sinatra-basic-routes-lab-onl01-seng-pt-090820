bd92d739a4203f189cbed818ed5feb0e8a9206ed
Michelle Ranly <meranly8@gmail.com>
2020-11-25 05:02:24 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -1,4 +1,16 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get '/name' do
+    "My name is Michelle"
+  end
+
+  get '/hometown' do
+    "My hometown is Kalamazoo, MI"
+  end
+
+  get '/favorite-song' do
+    "My favorite song is Heaven Knows I'm Miserable by The Smith's"
+  end
 end
